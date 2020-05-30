e030008ba957f3d520b951c385e275ee46e2c25a
Marlon Gomes <longe.gomes@gmail.com>
2020-05-30 19:36:34 -0300

    Minha Conta

--- site.php
+++ site.php
@@ -242,3 +242,55 @@ $app->post('/forgot/reset', function(){
 	$page = new Page();	
 	$page->setTpl('forgot-reset-success');
 });
+
+$app->get('/profile', function() {
+	User::verifyLogin(false);
+	$user = User::getFromSession();
+
+	$page = new Page;
+	$page->setTpl('profile', [
+		'user' => $user->getValues(),
+		'profileMsg' => User::getSuccess(),
+		'profileError' => User::getError()
+ 	]);
+});
+
+$app->post('/profile', function() {
+	User::verifyLogin(false);
+
+	if (!isset($_POST['desperson']) || 
+		!$_POST['desperson']) {
+			User::setError('Preencha seu nome');
+			header('Location: /profile');
+			exit;
+		}
+
+	if (!isset($_POST['desemail']) || 
+		!$_POST['desemail']) {
+			User::setError('Preencha seu email');
+			header('Location: /profile');
+			exit;
+		}
+
+	$user = User::getFromSession();
+
+	if ($_POST['desemail'] !== $user->getdesemail()) {
+		if (User::checkLoginExists($_POST['desemail'])) {
+			User::setError('Este email já está em uso.');
+			header('Location: /profile');
+			exit;
+		}
+	}
+
+	$_POST['inadmin'] = $user->getinadmin();
+	$_POST['despassword'] = $user->getdespassword();
+	$_POST['deslogin'] = $_POST['desemail'];
+	
+	$user->setValues($_POST);
+	$user->save();
+
+	User::setSuccess('Dados alterados com sucesso.');
+
+	header('Location: /profile');
+	exit;
+});
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -13,6 +13,7 @@ class User extends Model
     const SECRET_IV = 'HcodePhp7_Secret_IV';
     const ERROR = 'UserError';
     const ERROR_REGISTER = 'UserErrorRegister';
+    const SUCCESS = 'UseSuccess';
 
     public static function getFromSession()
     {
@@ -309,6 +310,26 @@ class User extends Model
     public static function clearError()
     {
         $_SESSION[User::ERROR] = NULL;
+    }
+
+    public static function setSuccess($msg)
+    {
+        $_SESSION[User::SUCCESS] = $msg;
+    }
+
+    public static function getSuccess()
+    {
+        $msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? 
+                    $_SESSION[User::SUCCESS] : '';
+
+        User::clearSuccess();
+
+        return $msg;
+    }
+
+    public static function clearSuccess()
+    {
+        $_SESSION[User::SUCCESS] = NULL;
     }
 
     public static function setErrorRegister($msg)
