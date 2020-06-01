de1203c623fcfb59651163b248d86b397bd5204e
Marlon Gomes <longe.gomes@gmail.com>
2020-06-01 15:46:35 -0300

    Admin - Categories (Pagination, Search)

--- admin-categories.php
+++ admin-categories.php
@@ -6,11 +6,38 @@ use \Hcode\Model\User;
 use \Hcode\Model\Product;
 
 $app->get('/admin/categories', function() {
-	$categories = Category::listAll();
+	User::verifyLogin();
+
+	$search = (isset($_GET['search'])) ? $_GET['search'] : '';
+	
+	$idpage = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+	
+	if (trim($search) != '') {
+		$categories = Category::getPagesSearch($search, $idpage);
+	} else {
+		$categories = Category::getPages($idpage);
+	}
+
+	$pages = [];
+
+	for ($x=0; $x < $categories['pages']; $x++) {
+		$build = [
+			'page' => $x+1
+		];
+
+		if (trim($search)) $build['search'] = $search;
+
+		array_push($pages, [
+			'href' => '/admin/users?' . http_build_query($build),
+			'text' => $x+1
+		]);
+	}	
 
 	$page = new PageAdmin;
 	$page->setTpl('categories', [
-		'categories' => $categories
+		'categories' => $categories['data'],
+		'search' => $search,
+		'pages' => $pages
 	]);
 });
 
--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -166,4 +166,50 @@ class Category extends Model
             ]
         );
     }
+
+    public static function getPages($page = 1, $itensPerPage = 10)
+    {
+        $start = ($page-1) * $itensPerPage;
+
+        $sql = new Sql;
+
+        $results = $sql->select(
+            "SELECT SQL_CALC_FOUND_ROWS * FROM tb_categories 
+            ORDER BY descategory 
+            LIMIT {$start}, {$itensPerPage}"
+        );
+
+        $total = $sql->select('SELECT FOUND_ROWS() AS total');
+
+        return [
+            'data' => $results,
+            'total' => (int) $total[0]['total'],
+            'pages' => ceil($total[0]['total'] / $itensPerPage)
+        ];
+    }
+
+    public static function getPagesSearch($search, $page = 1, $itensPerPage = 10)
+    {
+        $start = ($page-1) * $itensPerPage;
+
+        $sql = new Sql;
+
+        $results = $sql->select(
+            "SELECT SQL_CALC_FOUND_ROWS * FROM tb_categories 
+            WHERE descategory LIKE :search 
+            ORDER BY descategory 
+            LIMIT {$start}, {$itensPerPage}",
+            [
+                ':search' => "%{$search}%"
+            ]
+        );
+
+        $total = $sql->select('SELECT FOUND_ROWS() AS total');
+
+        return [
+            'data' => $results,
+            'total' => (int) $total[0]['total'],
+            'pages' => ceil($total[0]['total'] / $itensPerPage)
+        ];
+    }
 }
